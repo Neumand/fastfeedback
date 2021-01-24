f2b18b33d7aeee961d86937c61c6fd9a6be77870
David Neuman <david.m.neuman@gmail.com>
2021-01-24 18:21:13 -0500

    Creating new feedback

--- components/Feedback.js
+++ components/Feedback.js
@@ -3,11 +3,11 @@ import { format, parseISO } from "date-fns";
 
 const Feedback = ({ author, text, createdAt }) => {
   return (
-    <Box borderRadius={4} maxWidth="700px" w="full">
+    <Box mt={8} borderRadius={4} maxWidth="700px" w="full">
       <Heading size="sm" as="h3" mb={0} color="gray.900" fontWeight="medium">
         {author}
       </Heading>
-      <Text fontSize="sm" color="gray.400" mb={4}>
+      <Text fontSize="12px" color="gray.400" mb={4}>
         {format(parseISO(createdAt), "PPpp")}
       </Text>
       <Text color="gray.800" mb={4}>
--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,8 +1,53 @@
+import { useState, useRef } from "react";
+import { useRouter } from "next/router";
+import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+
 import Feedback from "@components/Feedback";
+import { useAuth } from "@lib/auth";
+import { createFeedback } from "@lib/db";
 import { getAllFeedback, getAllSites } from "@lib/db-admin";
 
 const SiteFeedback = ({ initialFeedback }) => {
-  return initialFeedback.map(feedback => <Feedback key={feedback.id} {...feedback} />);
+  const { user } = useAuth();
+  const router = useRouter();
+  const inputRef = useRef(null);
+  const [allFeedback, setAllFeedback] = useState(initialFeedback);
+
+  const { siteId } = router.query;
+
+  const onAddFeedback = e => {
+    e.preventDefault();
+
+    const newFeedback = {
+      author: user.name,
+      authorId: user.uid,
+      siteId,
+      text: inputRef.current.value,
+      status: "pending",
+      provider: user.provider,
+      createdAt: new Date().toISOString(),
+    };
+
+    setAllFeedback([newFeedback, ...allFeedback]);
+    createFeedback(newFeedback);
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" width="full" maxWidth="700px" margin="0 auto">
+      <Box as="form" onSubmit={onAddFeedback}>
+        <FormControl mt={8}>
+          <FormLabel htmlFor="comment">Comment</FormLabel>
+          <Input ref={inputRef} type="comment" id="comment"></Input>
+          <Button type="submit" size="sm" fontWeight="medium" mt={2}>
+            Add comment
+          </Button>
+        </FormControl>
+      </Box>
+      {allFeedback.map(feedback => (
+        <Feedback key={feedback.id} {...feedback} />
+      ))}
+    </Box>
+  );
 };
 
 export async function getStaticProps(context) {
